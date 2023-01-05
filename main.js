500b116b28bc573b840bec048cb162b6c3e04476
Aditya Pathak <adityap029@gmail.com>
2023-01-04 21:37:09 -0500

    added basic functionality for drawing

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 let sketchPad = document.querySelector(".sketchPad");
+let resetButton = document.getElementById("Reset Board<");
+console.log(resetButton);
 sketchPad.style.gridTemplateColumns = "repeat(16, 1fr)";
 sketchPad.style.gridTemplateRows = "repeat(16, 1fr)";
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 for (let i = 0; i < 256; i++) {
     let sketchPadSquare = document.createElement("div");
     sketchPadSquare.style.backgroundColor = "pink";
-    sketchPad.insertAdjacentElement("beforeend", sketchPadSquare);
-    sketchPadSquare.addEventListener("click", e =>{
-        sketchPadSquare.style.backgroundColor = "black";
-    });
+    sketchPadSquare.addEventListener("mousedown", colorSquare);
+    sketchPadSquare.addEventListener("mouseover", colorSquare);
+    sketchPad.appendChild(sketchPadSquare);
+
 }
+
+function colorSquare (e) {
+    if (e.type === 'mouseover' && !mouseDown) {
+        console.log("you are hovering!")
+        return
+    }
+    console.log("you have clicked a square!")
+    console.log( e.target.style.backgroundColor);
+    e.target.style.backgroundColor = "black";
+}
+
+
+
+
