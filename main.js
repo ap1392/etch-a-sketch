3d32ecca5c5f3c0a051519fa73a67a59f82b2217
Aditya Pathak <adityap029@gmail.com>
2023-01-05 15:33:16 -0500

    Added a slider for resizing the size of the sketchpad

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 let sketchPad = document.querySelector(".sketchPad");
-let resetButton = document.getElementById("Reset Board<");
-console.log(resetButton);
-sketchPad.style.gridTemplateColumns = "repeat(16, 1fr)";
-sketchPad.style.gridTemplateRows = "repeat(16, 1fr)";
+let slider = document.getElementById("myRange");
+let sliderValue = slider.value;
+console.log(sliderValue);
+
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
+initializeSketchPad(sliderValue);
 
-for (let i = 0; i < 256; i++) {
+
+
+document.getElementById("reset").onclick = resetSketchPad;
+
+function resetSketchPad () {
+    let allSquares = sketchPad.querySelectorAll("div");
+    allSquares.forEach((div) => div.style.backgroundColor = "pink");
+};
+
+slider.onchange = function() {
+    console.log("Hello world");
+    sliderValue = document.getElementById("myRange").value;
+    console.log(sliderValue);
+    initializeSketchPad(sliderValue);
+    resetSketchPad();
+  }
+
+
+function initializeSketchPad (boardSize) {
+    sketchPad.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
+    sketchPad.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`;
+    for (let i = 0; i < boardSize * boardSize; i++) {
     let sketchPadSquare = document.createElement("div");
     sketchPadSquare.style.backgroundColor = "pink";
     sketchPadSquare.addEventListener("mousedown", colorSquare);
     sketchPadSquare.addEventListener("mouseover", colorSquare);
     sketchPad.appendChild(sketchPadSquare);
-
+    }
 }
+
 
 function colorSquare (e) {
     if (e.type === 'mouseover' && !mouseDown) {
